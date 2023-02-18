8158bab5f3acfece6ea67f321c83a73f7fa530ab
varungujjar <varun@kenosis.in>
2023-02-18 20:07:01 +0530

    Redux conflict State Fixed

--- src/redux/reducers/pokemonReducer.js
+++ src/redux/reducers/pokemonReducer.js
@@ -1,13 +1,13 @@
 import CONFIG from '../../config';
 
-const pokemonReducer = (state, action) => {
+const pokemonReducer = (state = { loading: true }, action) => {
 	switch (action.type) {
 		case CONFIG.ACTION_POKEMON:
 			return { ...state, ...action.payload, loading: false };
 		case CONFIG.ACTION_POKEMON_LOADING:
 			return { ...state, loading: true };
 		default:
-			return { ...state, loading: true };
+			return state;
 	}
 };
 
--- src/redux/reducers/pokemonsReducer.js
+++ src/redux/reducers/pokemonsReducer.js
@@ -1,13 +1,13 @@
 import CONFIG from '../../config';
 
-const pokemonsReducer = (state, action) => {
+const pokemonsReducer = (state = { loading: true }, action) => {
 	switch (action.type) {
 		case CONFIG.ACTION_POKEMONS:
 			return { ...state, ...action.payload, loading: false };
 		case CONFIG.ACTION_POKEMONS_LOADING:
 			return { ...state, loading: true };
 		default:
-			return { ...state, loading: true };
+			return state;
 	}
 };
 
